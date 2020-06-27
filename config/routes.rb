2f49d11779c456fd3e778b37e9f74a0a94d9de75
Kohei Onodera <kohei.onodera@rakuten.com>
2020-06-28 06:24:19 +0900

    added banks controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :assets
+  resources :banks
 end
