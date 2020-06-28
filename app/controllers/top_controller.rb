45158b49429a49c3a73c86cb1f5135753554bc22
Kohei Onodera <kohei.onodera@rakuten.com>
2020-06-29 07:53:27 +0900

    modified date select and misc

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,5 @@
 class TopController < ApplicationController
   def index
-    @monthly_assets = Asset.group("date_format(occurred_at, '%Y-%m')").sum(:amount).map{|k,v| {month: k, amount: v}}
+    @monthly_assets = Asset.group("date_format(occurred_at, '%Y-%m')").sum(:amount).map{|k,v| {month: k, amount: v}}.sort{|a,b| b[:month] <=> a[:month] }
   end
 end
